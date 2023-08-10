6ccf8c619edec5ffc94912c11ff17f852658b62b
bishalniraula <51102345+bishalniraula@users.noreply.github.com>
2023-08-10 16:02:56 +0545

    Update AccessControllers.cs

--- Controllers/AccessControllers.cs
+++ Controllers/AccessControllers.cs
@@ -1,12 +1,1 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace AuthProject.Controllers
-{
-    public class AccessControllers : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
-}
+//
